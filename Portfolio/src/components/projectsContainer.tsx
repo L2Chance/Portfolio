3b0f5967691637b395c831e2f8ae0f3048365a35
L2Chance <Estudiandoyavanzando@gmail.com>
2025-05-25 19:37:47 -0300

    mejoras en la responsividad y avances en la segunda sección

---
+++ Portfolio/src/components/projectsContainer.tsx
@@ -0,0 +1,15 @@
+export default function ProjectContainer() {
+  return (
+    <>
+      <article
+        className="flex flex-col justify-center items-center w-full h-50 
+    bg-gradient-to-br from-white via-gray-200 to-gray-400 
+    dark:bg-gradient-to-br dark:from-[var(--dark)] dark:via-gray-800 dark:to-gray-900
+    rounded-2xl border-2
+  "
+      >
+        Hola
+      </article>
+    </>
+  );
+}
--- Portfolio/src/components/section-2/sectionTwo.tsx
+++ Portfolio/src/components/section-2/sectionTwo.tsx
@@ -1,41 +1,57 @@
+import TerminalContainer from "../terminalContainer.tsx";
 import Tool from "../Tool.tsx";
-import { FaHtml5 } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaReact,
+  FaPython,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { MdOutlineRestartAlt } from "react-icons/md";
+import { TbFileTypeSql, TbBrandReactNative } from "react-icons/tb";
+import { BiLogoTypescript } from "react-icons/bi";
+import ProjectContainer from "../projectsContainer.tsx";
 
 export default function SectionTwo() {
   return (
     <>
+      <div className="h-16"></div> {/*Separador*/}
+      {/*Contenedor general*/}
       <div
-        className="flex h-min-100 justify-center items-center mt-3 px-4"
+        className="flex flex-col md:flex-row justify-center gap-6 min-h-[85vh] pl-3 pr-3"
         data-aos="fade-up"
-        data-aos-duration="700"
         data-aos-delay="300"
       >
-        <section className="parent grid grid-cols-1 md:grid-cols-5 grid-rows-6 md:grid-rows-5 w-full max-w-[1200px] rounded-lg gap-4">
-          <div
-            className="div3 flex flex-col justify-start items-center col-span-1 md:col-span-3 row-span-4 md:row-span-5 border-2 
-                      bg-black/60 border-[var(--dark)] dark:border-[var(--white)] text-[var(--white)] p-4 rounded-lg animate-fade-left animate-duration-700 animate-delay-300 gap-2"
-          >
-            <h2 className="text-green-400 text-2xl text-shadow-black text-shadow-md/40">
-              PROYECTOS
-            </h2>
+        <section className="flex flex-col md:w-1/2 gap-1">
+          <TerminalContainer>
+            <p>Algunas técnologias que manejo</p>
+          </TerminalContainer>
+          {/* Contenedor de técnologias*/}
+          <div className="flex flex-wrap justify-center items-center border-2 border-[var(--dark)] dark:border-[var(--white)] text-[var(--white)] p-6 rounded-lg animate-fade-up animate-duration-700 animate-delay-300 gap-4 bg-black/60 w-full">
+            <Tool icono={FaHtml5} nombre="HTML"></Tool>
+            <Tool icono={FaCss3Alt} nombre="CSS"></Tool>
+            <Tool icono={FaJsSquare} nombre="JavaScript"></Tool>
+            <Tool icono={BiLogoTypescript} nombre="TypeScript"></Tool>
+            <Tool icono={FaNodeJs} nombre="NodeJS"></Tool>
+            <Tool icono={FaReact} nombre="ReactJS"></Tool>
+            <Tool icono={TbBrandReactNative} nombre="React Native"></Tool>
+            <Tool icono={SiTailwindcss} nombre="TailwindCSS"></Tool>
+            <Tool icono={FaPython} nombre="Python"></Tool>
+            <Tool icono={MdOutlineRestartAlt} nombre="ApiRest"></Tool>
+            <Tool icono={TbFileTypeSql} nombre="SQL"></Tool>
           </div>
-          <div
-            className="div2 flex flex-wrap justify-center items-center col-span-1 md:col-span-2 row-span-2 md:row-span-5 border-2 
-                      border-[var(--dark)] text-[var(--white)] bg-black/60 dark:border-[var(--white)] p-4 rounded-lg animate-fade-up 
-                      animate-duration-700 animate-delay-300 gap-2"
-          >
-            <h2 className="text-green-400 text-2xl text-shadow-black text-shadow-md/40">
-              TÉCNOLOGIAS
-            </h2>
-            <div className="flex justify-center flex-wrap gap-1">
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-              <Tool icono={FaHtml5} nombre="HTML5" />
-            </div>
+        </section>
+        <section className="flex flex-col md:w-1/2 gap-1">
+          <TerminalContainer>
+            <p>Mis proyectos personales</p>
+          </TerminalContainer>
+          {/*Contenedor de proyectos*/}
+          <div className="flex flex-wrap justify-center items-center border-2 border-[var(--dark)] dark:border-[var(--white)] text-[var(--white)] p-6 rounded-lg animate-fade-up animate-duration-700 animate-delay-300 gap-4 bg-black/60 w-full">
+            <ProjectContainer></ProjectContainer>
+            <ProjectContainer></ProjectContainer>
+            <ProjectContainer></ProjectContainer>
           </div>
         </section>
       </div>
